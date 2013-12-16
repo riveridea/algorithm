4de07d33134c6a7732a470bfa1da415261d24c6c
alex zhang <cingluar.alex@gmail.com>
2013-12-16 13:49:38 -0600

    adding emppty code for generateTree.cc

---
+++ leetcode/generateTrees/generateTrees.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+/*
+	Given n, generate all structurelly unique BST's that store all values in [1,n]
+*/
+
+
+
+/**
+ * Definition for binary tree*/
+struct TreeNode{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL){}
+};
+
+class Solution {
+public:
+	vector<TreeNode *> generateTrees(int n){
+
+	}
+};
+
+int main(){
+
+}
