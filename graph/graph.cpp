6545f7fc021ace5b57d2f2ce523fca9ec6e8247a
alex zhang <cingular.alex@gmail.com>
2017-05-01 21:51:02 -0400

    daily coding

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -1,1 +1,46 @@
 //Graph representation implementation
+#include "graph.h"
+
+//constructor
+graph::graph
+{
+    V = 0; //no vertices in the beginning
+    E = 0; 
+}
+
+~graph()
+{
+    //TODO will release the resources 
+}
+
+void graph::add_edge_only(int u, int v)
+{
+    //TODO update the edges only
+    //adjacent list will be update once we want to convert
+    //the edges to adjacnet list
+}
+
+void graph::add_edge_update(int u, int v)
+{
+    //TODO update the edges and also the adjacent lists 
+}
+
+bool graph::find_cycle()
+{
+    //TODO find the cycle, based on the DFS
+}
+
+bool graph::tsort()
+{
+    //TODO topological sort if no cycle is found 
+}
+
+//test code
+int main()
+{
+    graph g;
+
+    //g.add_edge
+    
+    return 0;
+}
--- graph/graph.h
+++ graph/graph.h
@@ -1,6 +1,46 @@
 // A C++ program to represent the graph by different ways: 
 // add by edges but represented by adjacenet lists
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#include <vector>
+#include <list>
 using namespace std;
 
+typedef struct adjList
+{
+    int value;
+    vector<int> adj_nodes;
+}adj_list;
 
+class graph
+{
+    //dynamic graph, no of vertices will grow when new edges are added
+    int V;
+    int E;
+
+    //representation of edges
+    vector<pair<int, int>> edges;
+
+    //representation of adjacent lists
+    vector<adj_list> adj_lists;
+
+protected:
+    int edges_2_adj_lists();
+
+public:
+    //constructor
+    graph(){}
+
+    //destructor, need release all the resource
+    //including the edges and adjacent lists
+    ~graph();
+
+    //add edge the graph
+    void add_edge_only(int u, int v);
+    void add_edge_update(int u, int v);
+
+    //detect the cycle for graph
+    bool find_cycle();
+
+    //topological sorting for the graph
+    bool tsort();
+}
