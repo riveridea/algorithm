dd6fae67ace4f5b9141b144d89ea2cabea7e4c9d
Changchun Zhang <riveridea@Changchuns-MacBook-Air.local>
2017-05-07 16:48:15 -0400

    week1 passsed

--- coursera_graph/connected_components/connected_components.cpp
+++ coursera_graph/connected_components/connected_components.cpp
@@ -4,10 +4,37 @@
 using std::vector;
 using std::pair;
 
+void dfs(vector<vector<int> > &adj, int v, vector<bool> &visited)
+{
+	visited[v] = true; //firstly mark it as visited
+
+	for(vector<int>::iterator itr = adj[v].begin();
+					itr != adj[v].end();
+					itr++)
+	{
+		if(visited[*itr] == false)
+			dfs(adj, *itr, visited);
+	}		
+}
+
 int number_of_components(vector<vector<int> > &adj) {
-  int res = 0;
-  //write your code here
-  return res;
+  	int res = 0;
+  	//write your code here
+  	//use dfs for each unvisisted node and increase the 
+  	//components number 
+  	int v = adj.size();
+  	vector<bool> visited(v, false);
+
+  	for(int i = 0; i < v; i++)
+  	{
+		if(visited[i] == false)
+		{
+			//only for unvisited node, after dfs, res is increased
+			dfs(adj, i, visited);
+			res++;
+		}		
+  	}
+  	return res;
 }
 
 int main() {
