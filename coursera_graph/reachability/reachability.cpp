edc574b79cb8f1ecf280762d0951d942696a3004
Changchun Zhang <riveridea@Changchuns-MacBook-Air.local>
2017-05-07 15:43:30 -0400

    commit the 1st version to be submitted

--- coursera_graph/reachability/reachability.cpp
+++ coursera_graph/reachability/reachability.cpp
@@ -4,10 +4,36 @@
 using std::vector;
 using std::pair;
 
+bool dfs_reach(vector<vector<int> > &adj, int x, int y, vector<bool> &visited);
 
 int reach(vector<vector<int> > &adj, int x, int y) {
   //write your code here
-  return 0;
+  //use DFS to determine if destination y can be reachble 
+  //from x
+  int v = adj.size();//the number of vertices
+  vector<bool> visited(v, false); 
+  
+  bool result = dfs_reach(adj, x, y, visited);
+  return result;
+}
+
+bool dfs_reach(vector<vector<int> > &adj, int x, int y, vector<bool> &visited)
+{
+    visited[x] = true;
+
+    if (x == y) return true;
+    for(vector<int>::iterator itr = adj[x].begin();
+		    itr != adj[x].end(); itr++)   
+    {
+    	if (visited[*itr] == false)
+		{
+	    	//check if we found y
+	    	if((*itr == y) || dfs_reach(adj, *itr, y, visited))
+				return true;
+		}
+    }
+    
+	return false;
 }
 
 int main() {
