9c37cb00b6c1fe1afc8955ae8fa57031fcc3efa9
Changchun Zhang <riveridea@Changchuns-MacBook-Air.local>
2017-05-07 17:02:44 -0400

    fix tab issues

--- coursera_graph/reachability/reachability.cpp
+++ coursera_graph/reachability/reachability.cpp
@@ -26,14 +26,14 @@ bool dfs_reach(vector<vector<int> > &adj, int x, int y, vector<bool> &visited)
 		    itr != adj[x].end(); itr++)   
     {
     	if (visited[*itr] == false)
-		{
-	    	//check if we found y
-	    	if((*itr == y) || dfs_reach(adj, *itr, y, visited))
-				return true;
-		}
+	{
+	    //check if we found y
+	    if((*itr == y) || dfs_reach(adj, *itr, y, visited))
+			return true;
+	}
     }
     
-	return false;
+    return false;
 }
 
 int main() {
