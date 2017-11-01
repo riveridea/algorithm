7c1488a38ded763f565aded1ef24e08bc2caf290
alexzh <cingular.alex@gmail.com>
2017-11-01 17:01:11 -0400

    not complete 273 integer to english

---
+++ leetcode/273.integer-to-english-words.cpp
@@ -0,0 +1,29 @@
+/*
+ * [273] Integer to English Words
+ *
+ * https://leetcode.com/problems/integer-to-english-words
+ *
+ * algorithms
+ * Hard (22.36%)
+ * Total Accepted:    47K
+ * Total Submissions: 210.3K
+ * Testcase Example:  '123'
+ *
+ * 
+ * Convert a non-negative integer to its english words representation. Given
+ * input is guaranteed to be less than 231 - 1.
+ * 
+ * 
+ * For example,
+ * 
+ * 123 -> "One Hundred Twenty Three"
+ * 12345 -> "Twelve Thousand Three Hundred Forty Five"
+ * 1234567 -> "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty
+ * Seven"
+ */
+class Solution {
+public:
+    string numberToWords(int num) {
+        
+    }
+};
