607fadfb276542ebf5eca600e810eed2a7e3f7cf
alexzh <cingular.alex@gmail.com>
2017-10-31 21:06:02 -0400

    accepted 33

--- leetcode/33.search-in-rotated-sorted-array.cpp
+++ leetcode/33.search-in-rotated-sorted-array.cpp
@@ -24,8 +24,8 @@ public:
     int search(vector<int>& nums, int target) {
     	//use binary search to narrow down the searching area         
     	int l = 0, r = nums.size()-1;
-	
-	while(l < r)
+
+	while(l <= r)
 	{
 	    int mid = l + (r - l)/2;
 	    //find the next searching area
@@ -35,12 +35,40 @@ public:
 	    //go ahead 
 	    if(nums[mid] > nums[r])
 	    { //roation is exactly right after mid
+		if(target < nums[mid] && target >= nums[l])
+		{
+		    r = mid - 1;
+		}
+		else
+		{
+		    l = mid + 1;
+		}
 	    
 	    }
 	    else if(nums[mid] < nums[l])
 	    { //roation is exactly before mid
-		if()
+		if(target > nums[mid] && target <= nums[r])
+		{
+		    l = mid + 1;		
+		}
+		else
+		{
+		    r = mid - 1;
+		}
+	    }
+	    else
+	    {
+		if(target < nums[mid])
+		{
+		    r = mid -1;
+		}
+		else
+		{
+		    l = mid + 1;
+		}
 	    }
 	}
+
+	return -1;
     }
 };
