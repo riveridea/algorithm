b12e82abaa66775426f1b8641cfc5e62fadb1fc8
alexzhang <cingular.alex@gmail.com>
2017-11-02 21:05:22 -0400

    draft 91 decode ways

---
+++ leetcode/91.decode-ways.cpp
@@ -0,0 +1,44 @@
+/*
+ * [91] Decode Ways
+ *
+ * https://leetcode.com/problems/decode-ways
+ *
+ * algorithms
+ * Medium (19.94%)
+ * Total Accepted:    142.8K
+ * Total Submissions: 716.3K
+ * Testcase Example:  '""'
+ *
+ * 
+ * A message containing letters from A-Z is being encoded to numbers using the
+ * following mapping:
+ * 
+ * 
+ * 
+ * 'A' -> 1
+ * 'B' -> 2
+ * ...
+ * 'Z' -> 26
+ * 
+ * 
+ * 
+ * Given an encoded message containing digits, determine the total number of
+ * ways to decode it.
+ * 
+ * 
+ * 
+ * For example,
+ * Given encoded message "12",
+ * it could be decoded as "AB" (1 2) or "L" (12).
+ * 
+ * 
+ * 
+ * The number of ways decoding "12" is 2.
+ * 
+ */
+class Solution {
+public:
+    int numDecodings(string s) {
+        
+    }
+};
