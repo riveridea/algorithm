14ab8a0c24361dcc92db8ec45f3c864ae93d3cbe
alexzh <cingular.alex@gmail.com>
2017-11-03 17:45:14 -0400

    draft 13

---
+++ leetcode/13.roman-to-integer.cpp
@@ -0,0 +1,21 @@
+/*
+ * [13] Roman to Integer
+ *
+ * https://leetcode.com/problems/roman-to-integer
+ *
+ * algorithms
+ * Easy (46.56%)
+ * Total Accepted:    184.2K
+ * Total Submissions: 395.4K
+ * Testcase Example:  '"DCXXI"'
+ *
+ * Given a roman numeral, convert it to an integer.
+ * 
+ * Input is guaranteed to be within the range from 1 to 3999.
+ */
+class Solution {
+public:
+    int romanToInt(string s) {
+        
+    }
+};
