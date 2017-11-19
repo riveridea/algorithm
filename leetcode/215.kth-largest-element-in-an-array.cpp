67c9932b8c6cb2cc43518c5b8a564696f9887674
Changchun Zhang <riveridea@Changchuns-Air.fios-router.home>
2017-11-19 14:07:49 -0500

    accepted 215 kth largest element in an array by Quick Sort

--- leetcode/215.kth-largest-element-in-an-array.cpp
+++ leetcode/215.kth-largest-element-in-an-array.cpp
@@ -23,8 +23,50 @@
  * test cases.
  */
 class Solution {
+    //return the positon where the pivot final sits
+    int partition(vector<int>& nums, int left, int right)
+    {
+	int pivot = nums[left];
+	int l = left + 1, r = right;	
+	while(l <= r)
+	{
+	    //if l and r both conflict, then swap and GO AHEAD
+	    //else if l meets JUST go ahead, r meets JUST go ahead
+	    //no meets, stay 
+	    if(nums[l] < pivot && nums[r] > pivot)
+	    {
+		swap(nums[l++], nums[r--]);
+	    }
+
+	    if(nums[l] >= pivot) l++;
+	    if(nums[r] <= pivot) r--;
+	}
+	swap(nums[left], nums[r]);
+	return r;
+    }
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        
+	//use sorting, the idea is that the K th largest element should be in the k-1 index
+	//but you don't need to do fully sorting
+	//use quick sorting, each time use a pivot, then partion the elements
+	//if the pivot is exactly the k-1 position, return the pivot
+	//otherwise, if the pivot is at position larger than k-1 position,
+	//it means the answer is in the left partition, 
+	//else the answer is in the right partition
+	int left = 0, right = nums.size() - 1;
+	while(1)
+	{
+	    int pos = partition(nums, left, right);
+
+	    if(pos == k-1) return nums[pos];
+	    else if(pos > k-1) 
+	    {
+		right = pos - 1;
+	    }
+	    else
+	    {
+		left = pos + 1;
+	    }
+	}
     }
 };
