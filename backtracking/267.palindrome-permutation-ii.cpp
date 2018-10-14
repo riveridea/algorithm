922085ac726a2437425c12511143c5197b321818
alex on linux obi <cingular.alex@gmail.com>
2018-10-14 15:11:05 -0700

    new backtracking directory

---
+++ backtracking/267.palindrome-permutation-ii.cpp
@@ -0,0 +1,34 @@
+/*
+ * [267] Palindrome Permutation II
+ *
+ * https://leetcode.com/problems/palindrome-permutation-ii/description/
+ *
+ * algorithms
+ * Medium (32.49%)
+ * Total Accepted:    21.2K
+ * Total Submissions: 65.3K
+ * Testcase Example:  '"aabb"'
+ *
+ * Given a string s, return all the palindromic permutations (without
+ * duplicates) of it. Return an empty list if no palindromic permutation could
+ * be form.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "aabb"
+ * Output: ["abba", "baab"]
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "abc"
+ * Output: []
+ * 
+ */
+class Solution {
+public:
+    vector<string> generatePalindromes(string s) {
+        
+    }
+};
