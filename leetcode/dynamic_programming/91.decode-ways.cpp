b95414fa4b3a535e29c2d4d2a157fd0ee50ca75d
alex on linux obi <cingular.alex@gmail.com>
2018-10-30 19:06:30 -0700

    91 decode ways rolling array

--- leetcode/dynamic_programming/91.decode-ways.cpp
+++ leetcode/dynamic_programming/91.decode-ways.cpp
@@ -42,6 +42,29 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        
+	//this is vairant of climing ladders
+	//everytime you either move 1 step or move 2 steps
+	//f[n] represents the maximum ways of decoding to reach step n, but not including n yet
+	//we can reach f[n] from n-1 or n-2,
+	//thus f[n] = f[n-1] + f[n-2] 
+	//using rolling array, like cur = cur + pre;
+	//the difference is sometimes for the s[i-1] == '0' or s[i-2][i-1] > "2,6"
+	//we can say we never reach this f[i-1] or f[i-2], thus cur = 0, pre = 0
+	if(s.length() == 0 || s[0] == '0' ) return 0;
+
+	int pre = 0; //f[i-2]
+	int cur = 1; //f[i-1]
+	for(int i = 1; i <= s.length(); i++) 
+	{
+	    if (s[i-1] ==  '0') cur = 0;
+	    if (i < 2 || !(s[i-2] == '1' || (s[i-2] == '2' && s[i-1] <= '6'))) pre = 0;
+
+	    int temp = cur;
+	    cur = cur + pre;
+	    pre = temp;
+
+	    cout << "f[" << i << "], " << "cur = " << cur << endl;
+	}
+        return cur; //f[n] represents the final result, n = s.length, means we finished all n chars and reached the end after s[n-1]
     }
 };
