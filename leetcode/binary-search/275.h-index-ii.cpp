254fd84ab261d180982e1d64d76a1703abe2f88b
alex on linux obi <cingular.alex@gmail.com>
2018-11-09 11:50:02 -0800

    h-index II binary search

---
+++ leetcode/binary-search/275.h-index-ii.cpp
@@ -0,0 +1,71 @@
+/*
+ * [275] H-Index II
+ *
+ * https://leetcode.com/problems/h-index-ii/description/
+ *
+ * algorithms
+ * Medium (35.03%)
+ * Total Accepted:    71.9K
+ * Total Submissions: 205.2K
+ * Testcase Example:  '[0,1,3,5,6]'
+ *
+ * Given an array of citations sorted in ascending order (each citation is a
+ * non-negative integer) of a researcher, write a function to compute the
+ * researcher's h-index.
+ * 
+ * According to the definition of h-index on Wikipedia: "A scientist has index
+ * h if h of his/her N papers have at least h citations each, and the other N −
+ * h papers have no more than h citations each."
+ * 
+ * Example:
+ * 
+ * 
+ * Input: citations = [0,1,3,5,6]
+ * Output: 3 
+ * Explanation: [0,1,3,5,6] means the researcher has 5 papers in total and each
+ * of them had 
+ * ⁠            received 0, 1, 3, 5, 6 citations respectively. 
+ * Since the researcher has 3 papers with at least 3 citations each and the
+ * remaining 
+ * two with no more than 3 citations each, her h-index is 3.
+ * 
+ * Note:
+ * 
+ * If there are several possible values for h, the maximum one is taken as the
+ * h-index.
+ * 
+ * Follow up:
+ * 
+ * 
+ * This is a follow up problem to H-Index, where citations is now guaranteed to
+ * be sorted in ascending order.
+ * Could you solve it in logarithmic time complexity?
+ * 
+ * 
+ */
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+	//binary search 
+	//find the largest index that meets
+	//citations[i] >= size - i
+	int start = 0;
+	int size = citations.size(), end = size;
+	if(end == 0) return 0;
+
+	while(start < end)
+	{
+	    int mid = start + (end - start)/2;    
+	    if(citations[mid] < size - mid)// this index does not meet h-index
+	    {
+		start = mid +1;
+	    }
+	    else // a valid index but need to continue to find the max
+	    {
+		end = mid;	
+	    }
+	}
+
+	return size - end;
+    }
+};
