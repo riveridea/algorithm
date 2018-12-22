1386aac9bbc97c9ae1621370b4ecf3ed552d7a8d
riveridea <cingular.alex@gmail.com>
2018-12-22 12:34:13 -0800

    328 odd-even linked list, two pointers

---
+++ leetcode/linked-list/328.odd-even-linked-list.cpp
@@ -0,0 +1,108 @@
+/*
+ * @lc app=leetcode id=328 lang=cpp
+ *
+ * [328] Odd Even Linked List
+ *
+ * https://leetcode.com/problems/odd-even-linked-list/description/
+ *
+ * algorithms
+ * Medium (47.22%)
+ * Total Accepted:    122.7K
+ * Total Submissions: 259.9K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * Given a singly linked list, group all odd nodes together followed by the
+ * even nodes. Please note here we are talking about the node number and not
+ * the value in the nodes.
+ * 
+ * You should try to do it in place. The program should run in O(1) space
+ * complexity and O(nodes) time complexity.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 1->2->3->4->5->NULL
+ * Output: 1->3->5->2->4->NULL
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 2->1->3->5->6->4->7->NULL
+ * Output: 2->3->6->7->1->5->4->NULL
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * The relative order inside both the even and odd groups should remain as it
+ * was in the input.
+ * The first node is considered odd, the second node even and so on ...
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+	//ok, this problem can be resolved by myself withou looking at the solution
+	//maintain two pointers of odd_head, even_head, and the curr_odd, curr_even
+	//and  update curr_odd and curr_even every time while going forward
+	if(!head) return nullptr;
+	ListNode* odd_head = head;	
+	ListNode* even_head = head->next;
+	ListNode* curr_odd = odd_head;
+	ListNode* curr_even = even_head;
+
+	while(curr_even && curr_even->next)
+	{
+	    curr_odd->next = curr_even->next;
+	    curr_odd = curr_odd->next;
+	    if(curr_odd) 
+	    {
+		curr_even->next = curr_odd->next;
+		curr_even = curr_even->next;
+	    }
+	}
+	curr_odd->next = even_head;
+	return odd_head;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
