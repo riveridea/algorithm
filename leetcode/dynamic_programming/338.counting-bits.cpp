15bca13957cbae7fcab01f1fb00eb6042c3450ee
riveridea <cingular.alex@gmail.com>
2018-12-22 19:37:30 -0800

    338 counting bits, using the 2^area to plus 1 for correspoding previous areas

--- leetcode/dynamic_programming/338.counting-bits.cpp
+++ leetcode/dynamic_programming/338.counting-bits.cpp
@@ -35,6 +35,38 @@
 class Solution {
 public:
     vector<int> countBits(int num) {
-        
+	//I here use a clear method,not the i&(i-1) to update, as that is a little hard to understand
+	//we see the number in different areas
+	//1. 0 - 2^0
+	//2. 2^1 - 2^2-1
+	//3. 2^2 - 2^3-1
+	//4. 2^3 - 2^4-1
+	//5. 2^4 - 2^5-1
+	//etc.
+	//it is easy to find the number of integers of area i is the sum of all its previous areas
+	//also, the count of bits for a number in area i is corresponding to the same sequence of
+	//of the all preivous areas plus 1. 
+	//we can define a vector<int> for each area, we add the count bits for these numbers
+	//then the next area can use the count in the current result, using a indicator from the 
+	//begin of result to the latest one
+	if (num == 0) return {0};
+	if (num == 1) return {0, 1};
+	if (num == 2) return {0, 1, 1};
+	vector<int> res{0, 1, 1, 2};
+
+	int area = 2; 
+	int idx = 4;
+	while(idx <= num)
+	{
+	    //process each area each time
+	    int j = 0;
+	    for(idx = pow(2, area); idx < pow(2, area+1); ++idx, ++j)
+	    {
+		if(idx > num) break;
+		res.push_back(res[j] + 1);
+	    }
+	    area++;	
+	}
+	return res;
     }
 };
