9ebbf763ba3e5589f269c6c54fd31b2fe1d53b5c
changczh <you@example.com>
2019-10-11 05:53:52 -0400

    1048 dp

---
+++ leetcode/dynamic_programming/1048.longest-string-chain.cpp
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode id=1048 lang=cpp
+ *
+ * [1048] Longest String Chain
+ *
+ * https://leetcode.com/problems/longest-string-chain/description/
+ *
+ * algorithms
+ * Medium (50.76%)
+ * Total Accepted:    15.9K
+ * Total Submissions: 31.4K
+ * Testcase Example:  '["a","b","ba","bca","bda","bdca"]'
+ *
+ * Given a list of words, each word consists of English lowercase letters.
+ * 
+ * Let's say word1 is a predecessor of word2 if and only if we can add exactly
+ * one letter anywhere in word1 to make it equal to word2.  For example, "abc"
+ * is a predecessor of "abac".
+ * 
+ * A word chain is a sequence of words [word_1, word_2, ..., word_k] with k >=
+ * 1, where word_1 is a predecessor of word_2, word_2 is a predecessor of
+ * word_3, and so on.
+ * 
+ * Return the longest possible length of a word chain with words chosen from
+ * the given list of words.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: ["a","b","ba","bca","bda","bdca"]
+ * Output: 4
+ * Explanation: one of the longest word chain is "a","ba","bda","bdca".
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * 1 <= words.length <= 1000
+ * 1 <= words[i].length <= 16
+ * words[i] only consists of English lowercase letters.
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+class Solution {
+public:
+    static bool compare(string& a, string& b){
+        return a.size() < b.size();
+    }
+    int longestStrChain(vector<string>& words) {
+        //this problem is not hard
+        //think about soring the words by length,
+        //then any shorter words could be predessor of the longer one
+        //use a hashmap<string, int> to record the 
+        int n = words.size();
+        if(!n) return 0;
+
+        unordered_map<string, int> dp;
+        sort(words.begin(), words.end(), compare);
+      
+        int res = 0;
+        for(auto w: words){
+            int best = 0;
+            for(int i = 0; i < w.length(); ++i){
+                string pre = w.substr(0, i) + w.substr(i+1);
+                best = max(best, dp[pre]+1);
+            }
+            dp[w] = best;
+            res = max(res, best);
+        }
+        //the answer is not dp[words[n-1]]!!
+        return res;
+    }
+};
