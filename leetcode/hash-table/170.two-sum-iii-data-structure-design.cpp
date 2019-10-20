f68723711b03a4d2ca8523fa1e2dd002d11cd66f
riveridea <cingular.alex@gmail.com>
2019-10-20 13:52:51 -0400

    170 easy but pay attention to corner casesgit add .

---
+++ leetcode/hash-table/170.two-sum-iii-data-structure-design.cpp
@@ -0,0 +1,64 @@
+/*
+ * [170] Two Sum III - Data structure design
+ *
+ * https://leetcode.com/problems/two-sum-iii-data-structure-design/description/
+ *
+ * algorithms
+ * Easy (31.80%)
+ * Total Accepted:    67.1K
+ * Total Submissions: 211.2K
+ * Testcase Example:  '["TwoSum","add","add","add","find","find"]\n[[],[1],[3],[5],[4],[7]]'
+ *
+ * Design and implement a TwoSum class. It should support the following
+ * operations: add and find.
+ * 
+ * add - Add the number to an internal data structure.
+ * find - Find if there exists any pair of numbers which sum is equal to the
+ * value.
+ * 
+ * Example 1:
+ * 
+ * 
+ * add(1); add(3); add(5);
+ * find(4) -> true
+ * find(7) -> false
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * add(3); add(1); add(2);
+ * find(3) -> true
+ * find(6) -> false
+ * 
+ */
+class TwoSum {
+	unordered_map<int, int> m;
+public:
+    /** Initialize your data structure here. */
+    TwoSum() {
+        
+    }
+    
+    /** Add the number to an internal data structure.. */
+    void add(int number) {
+		m[number]++;
+    }
+    
+    /** Find if there exists any pair of numbers which sum is equal to the value. */
+    bool find(int value) {
+		for(auto p : m){
+			int d1 = p.first;
+			int d2 = value - d1;
+			if(d1 == d2 && m[d1] > 1 || d1 != d2 && m.count(d2)) return true;
+		}
+		return false;
+    }
+};
+
+/**
+ * Your TwoSum object will be instantiated and called as such:
+ * TwoSum* obj = new TwoSum();
+ * obj->add(number);
+ * bool param_2 = obj->find(value);
+ */
